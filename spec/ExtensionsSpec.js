bb813a47e1d78a988d8c5910aec4162f1b5aa1f6
Nathan E. Fairchild <natefair@tellme.com>
2012-06-15 13:09:38 -0700

    Adding javascript testing

---
+++ spec/ExtensionsSpec.js
@@ -0,0 +1,143 @@
+describe('extensions.js', function () {
+
+  // Date.addDays extension
+  describe('Date.addDays', function () {
+    it('adds 1 to 31 days to the current date', function () {
+      var now = Date.now(), date, msec, days;
+      for (days = 1; days <= 31; days += 1) {
+        date = new Date(now);
+        msec = date.getTime();
+        expect(date.addDays(days).getTime()).toEqual(msec + days * 24 * 60 * 60 * 1000);
+      }
+    });
+  });
+
+  // Date.toShortDate extension
+  describe('Date.toShortDate', function () {
+    it('returns "Jan 1, 1970" for "01/01/1970"', function () {
+      var date = new Date(Date.parse('01/01/1970'));
+      expect(date.toShortDate()).toEqual('Jan 1, 1970');
+    });
+  });
+
+  // Date.getDayOfWeek extension
+  describe('Date.getDayOfWeek', function () {
+    it('returns "Monday" for "Aug 1, 2011"', function () {
+      var date = new Date(Date.parse('Aug 1, 2011'));
+      expect(date.getDayOfWeek()).toEqual('Monday');
+    });
+  });
+
+  // Date.getDOW extension
+  describe('Date.getDOW', function () {
+    it('returns "Mon" for "Aug 1, 2011"', function () {
+      var date = new Date(Date.parse('Aug 1, 2011'));
+      expect(date.getDOW()).toEqual('Mon');
+    });
+  });
+
+  // Date.getMonthLong extension
+  describe('Date.getMonthLong', function () {
+    it('returns "August" for "Aug 1, 2011"', function () {
+      var date = new Date(Date.parse('Aug 1, 2011'));
+      expect(date.getMonthLong()).toEqual('August');
+    });
+  });
+
+  // Date.getMonthShort extension
+  describe('Date.getMonthShort', function () {
+    it('returns "Aug" for "August 1, 2011"', function () {
+      var date = new Date(Date.parse('August 1, 2011'));
+      expect(date.getMonthShort()).toEqual('Aug');
+    });
+  });
+
+  // Date.parseRelative extension
+  describe('Date.parseRelative', function () {
+    it('returns today\'s short date for "today"', function () {
+      var today = new Date();
+      expect(Date.parseRelative('today')).toEqual(today.toShortDate());
+    });
+    it('returns tomorrowi\'s short date for "tomorrow"', function () {
+      var today = new Date();
+      expect(Date.parseRelative('tomorrow')).toEqual(today.addDays(1).toShortDate());
+    });
+    it('returns the day after tomorrow\'s short date for "the day after"', function () {
+      var today = new Date();
+      expect(Date.parseRelative('the day after')).toEqual(today.addDays(2).toShortDate());
+    });
+  });
+
+  // String.isNumber extension
+  describe('String.isNumber', function () {
+    it('says that floats are numbers', function () {
+      var arr = ['0.01', '1.23', '-100.05', '123.4567'], i;
+      for (i = 0; i < arr.length; i += 1) {
+        expect(arr[i].isNumber()).toBe(true);
+      }
+    });
+    it('says "Hello" is not a number', function () {
+       expect('Hello'.isNumber()).toBe(false);
+    });
+    it('says "2012/06/05" is not a number', function () {
+       expect('2012/06/05'.isNumber()).toBe(false);
+    });
+  });
+
+  // Number.formatAsCurrency extension
+  describe('Number.formatAsCurrency', function () {
+    it('converts 14.9 to "14.90"', function () {
+      expect(14.9.formatAsCurrency()).toBe('14.90');
+    });
+    it('converts -5.2 to "-5.20"', function () {
+      expect((-5.2).formatAsCurrency()).toBe('-5.20');
+    });
+    it('converts Infinity to "0.00"', function () {
+       expect(Infinity.formatAsCurrency()).toBe('0.00');
+    });
+    it('converts NaN to "0.00"', function () {
+       expect(NaN.formatAsCurrency()).toBe('0.00');
+    });
+  });
+
+  // String.formatAsCurrency extension
+  describe('String.formatAsCurrency', function () {
+    it('converts "14.9" to "14.90"', function () {
+      expect('14.9'.formatAsCurrency()).toBe('14.90');
+    });
+    it('converts "-5.2" to "-5.20"', function () {
+      expect('-5.2'.formatAsCurrency()).toBe('-5.20');
+    });
+  });
+
+  // String.capitalize extension
+  describe('String.capitalize', function () {
+    it('converts "chase checking" to "Chase checking"', function () {
+      expect('chase checking'.capitalize()).toEqual('Chase checking');
+    });
+  });
+
+  // Global isNumber function
+  describe('isNumber', function () {
+    it('says that integers are numbers', function () {
+      for (var i = -10; i <= 10; i += 1) {
+        expect(isNumber(i)).toBe(true);
+      }
+    });
+    it('says that floats are numbers', function () {
+      var arr = [0.01, 1.23, -100.05, 123.4567], i;
+      for (i = 0; i < arr.length; i += 1) {
+        expect(isNumber(arr[i])).toBe(true);
+      }
+    });
+    it('says Infinity is not a number', function () {
+       expect(isNumber(Infinity)).toBe(false);
+    });
+    it('says "Hello" is not a number', function () {
+       expect(isNumber('Hello')).toBe(false);
+    });
+    it('says "2012/06/05" is not a number', function () {
+       expect(isNumber('2012/06/05')).toBe(false);
+    });
+  });
+});
